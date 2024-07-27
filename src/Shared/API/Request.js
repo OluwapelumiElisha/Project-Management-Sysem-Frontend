15903f0332222e21d4cbc153f7e5ed09bb1feebe
Oluwapelumielisha <elishaoluwapelumi38@gmail.com>
2024-07-27 07:48:14 +0100

    PMS React

--- src/Shared/API/Request.js
+++ src/Shared/API/Request.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 
-const baseURL = 'http://localhost:4000'
+const baseURL = 'https://project-management-system-dcbh.onrender.com'
 
 
 export const publicRequest = axios.create({
